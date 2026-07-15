3afe4fea3b33f4bb86cc65d7df46022827305f24
Ayush Paliwal <ayushpaliwal1920@gmail.com>
2026-07-16 00:19:04 +0530

    Time: 8 ms (30.92%), Space: 47.2 MB (46.32%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -3,19 +3,19 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
 
-        int min = Integer.MAX_VALUE;
+        // int min = Integer.MAX_VALUE;
 
-        int[][] dp = new int[n][m];
+        // int[][] dp = new int[n][m];
 
-        for(int[] row : dp){
-            Arrays.fill(row , Integer.MAX_VALUE);
-        }
+        // for(int[] row : dp){
+        //     Arrays.fill(row , Integer.MAX_VALUE);
+        // }
 
-        for(int j = 0 ; j < m ; j++){
-            min = Math.min(min , helperMemo(n-1 , j , matrix , dp));
-        }
+        // for(int j = 0 ; j < m ; j++){
+        //     min = Math.min(min , helperMemo(n-1 , j , matrix , dp));
+        // }
 
-        return min;
+        return helperTabulation1(matrix);
     }
 
     // Recursion : 
@@ -49,6 +49,48 @@ class Solution {
 
         return dp[i][j] = Math.min(up , Math.min(upleft , upright));
     }
+
+    // Tabulation : 
+
+    public static int helperTabulation1(int[][] matrix) {
+
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[][] dp = new int[n][m];
+
+        // Base case :
+
+        for (int c = 0; c < m; c++) {
+            dp[0][c] = matrix[0][c];
+        }
+
+        for (int r = 1; r < n; r++) {
+            for (int c = 0; c < m; c++) {
+                int upleft = (int) 1e9;
+                int upright = (int) 1e9;
+                int up = matrix[r][c] + dp[r - 1][c];
+                if (c > 0) {
+                    upleft = matrix[r][c] + dp[r - 1][c - 1];
+                }
+                if (c < m - 1) {
+                    upright = matrix[r][c] + dp[r - 1][c + 1];
+                }
+
+                dp[r][c] = Math.min(up, Math.min(upleft, upright));
+            }
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for (int c = 0; c < m; c++) {
+            min = Math.min(min, dp[n - 1][c]);
+        }
+
+        return min;
+    }
+
 
 
 }
