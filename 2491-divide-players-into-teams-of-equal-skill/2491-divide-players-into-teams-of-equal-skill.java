7c54672d218aa1f8a90cbe5dc398aba9664f6d3d
Ayush Paliwal <ayushpaliwal1920@gmail.com>
2026-08-08 12:21:36 +0530

    Time: 21 ms (42.14%), Space: 77.1 MB (13.92%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.java
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.java
@@ -3,30 +3,20 @@ class Solution {
         Arrays.sort(skill);
         int n = skill.length;
 
-        if(n%2 != 0) return -1;
-
-        Map<Integer, Integer> mp = new HashMap<>();
-
-        int i = 0 ;
+        int i = 0 ; 
         int j = n-1;
-
-        while(i <= j){
-            mp.put(skill[i]+skill[j] , mp.getOrDefault(skill[i]+skill[j] , 0)+1);
-            i++;
-            j--;
-        }
-
-        if(mp.size() > 1) return -1;
-
-        i = 0;
-        j = n-1;
-
         long ans = 0;
 
-        while(i<j){
-            ans += skill[i]*skill[j];
+        int condn = skill[0] + skill[n-1];
+
+        while(i < j){
+            if(skill[i] + skill[j] != condn){
+                return -1;
+            }
+
+            ans += (long) skill[i] * skill[j];
             i++;
-            j--; 
+            j--;
         }
 
         return ans;
