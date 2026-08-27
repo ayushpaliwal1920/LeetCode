89bcd6d94025e6895374ecbf80d22fdbeed665e8
Ayush Paliwal <ayushpaliwal1920@gmail.com>
2026-08-27 18:41:47 +0530

    Time: 11 ms (62.18%), Space: 57.1 MB (29.15%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,12 +1,12 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[n][amount+1];
+        // int[][] dp = new int[n][amount+1];
 
-        for(int[] row : dp){
-            Arrays.fill(row , -1);
-        }
-        return helper(amount , coins , n-1 , dp);
+        // for(int[] row : dp){
+        //     Arrays.fill(row , -1);
+        // }
+        return helpertabulation(amount , coins);
     }
     public int helper(int amount , int[] arr , int n , int[][] dp){
         if(n == 0){
@@ -22,6 +22,31 @@ class Solution {
             pick = helper(amount - arr[n] , arr , n , dp);
         }
         return  dp[n][amount] = pick + notpick;
+    }
+
+    public int helpertabulation(int target , int[] arr){
+        int n = arr.length;
+
+        int[][] dp = new int[n][target+1];
+
+        for(int t = 0 ; t <= target ; t++){
+            dp[0][t] = (t % arr[0] == 0) ? 1  : 0;
+        }
+
+        for(int i = 1 ; i< n ; i++){
+            for(int t = 0 ; t <= target ; t++){
+                int notpick = dp[i-1][t];
+                int pick = 0;
+                if(arr[i] <= t){
+                    pick = dp[i][t - arr[i]];
+                }
+
+                dp[i][t] = pick + notpick;
+            }
+        }
+
+        return dp[n-1][target];
+
     }
 
 
