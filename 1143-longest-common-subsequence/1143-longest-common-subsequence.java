53913ee3ee0cef5bb2e9288285689b1793e5a302
Ayush Paliwal <ayushpaliwal1920@gmail.com>
2026-09-06 19:54:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -2,13 +2,13 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int n1 = text1.length();
         int n2 = text2.length();
-        int[][] dp = new int[n1+1][n2+1];
+        // int[][] dp = new int[n1+1][n2+1];
 
-        for(int[] r : dp ){
-            Arrays.fill(r, -1);
-        }
+        // for(int[] r : dp ){
+        //     Arrays.fill(r, -1);
+        // }
 
-        return helperMemo1(text1, text2, n1 - 1, n2 - 1 , dp);
+        return helperTabulation1(text1, text2, n1, n2);
     }
 
     public int helper1(String s1 , String s2 , int n1 , int n2){
@@ -42,5 +42,24 @@ class Solution {
 
         return dp[n1][n2] = Math.max(helperMemo1(s1, s2, n1 - 1, n2 , dp), helperMemo1(s1, s2, n1, n2 - 1 , dp));
     }
+
+    // Tabulation : 
+
+    public static  int helperTabulation1(String s1 , String s2 , int n1 , int n2){
+        int[][] dp = new int[n1+1][n2+1];
+
+        for(int i = 1 ; i<= n1 ; i++){
+            for(int j = 1 ; j<=n2 ; j++ ){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j] ,dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[n1][n2];
+    }
+
 
 }
